e79d86227a7793e011f839d23f7faf2ddbdbe783
dbobzin <dbobzin@ilstu.edu>
2023-02-16 17:35:08 -0600

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1 className="container">Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <p>put in bootstrap </p>
+      <p>images in public</p>
+      <p>break images down into components</p>
     </div>
   );
 }
